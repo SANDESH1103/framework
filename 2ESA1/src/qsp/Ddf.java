f1b89ca7abf4773c52c8b8a633ae420499c70bd5
Sandesh <ersandesh11@gmail.com>
2022-04-13 20:37:45 +0530

    new

--- 2ESA1/src/qsp/Ddf.java
+++ 2ESA1/src/qsp/Ddf.java
@@ -3,8 +3,7 @@ package qsp;
 public class Ddf {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
--- 2ESA1/src/qsp/FileUpload.java
+++ 2ESA1/src/qsp/FileUpload.java
@@ -10,19 +10,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FileUpload {
 	static {
 
-		System.setProperty("webdriver.chrome.driver", "D:\\Selinium\\2ESA1\\driver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "D:\\framework\\2ESA1\\driver\\chromedriver.exe");
 	}
 
 
 	public static void main(String[] args) {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("http://127.0.0.1:5500/File.HTML");
-		WebElement chooseFile=driver.findElement(By.id("a1"));
-		File f1=new File("./cv/resume12.docx");
-		String absPath=f1.getAbsolutePath();
-		System.out.println(absPath);
-		chooseFile.sendKeys(absPath);
+		driver.get("file:///C:/Users/Sandesh%20Sonawane/Desktop/FileUpload.HTML");
+		driver.findElement(By.id("p1")).sendKeys("D:\\Selinium\\Books.xlsx");
+		/*
+		 * File f1=new File("./cv/resume12.docx"); String absPath=f1.getAbsolutePath();
+		 * System.out.println(absPath); chooseFile.sendKeys(absPath);
+		 */
 
 	}
 
